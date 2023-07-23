2c52efde74132e8aefdcd927a52b1e464d67f2d9
Kostyantyn Haman <kostyantyn.haman@eglobal-group.com>
2023-07-23 19:10:59 +0300

    Add make not success command

---
+++ app/Console/Commands/MakeNotSuccess.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Model\Coin;
+use Illuminate\Console\Command;
+
+class
+MakeNotSuccess extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'not:success {from}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        Coin::where('number', '>', $this->argument('from'))->chunk(500, function ($customers) {
+            foreach ($customers as $customer) {
+                $customer->not_success = true;
+                $customer->save();
+            }
+        });
+    }
+}
